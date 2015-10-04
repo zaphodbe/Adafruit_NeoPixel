300de21bdfa50ad256890152ef5de9745410e395
Christopher Coley <ccoley@zaphod.com>
2015-10-04 10:46:45 -0700

    Added support to pass pixel array to show which defaults to internal array

--- Adafruit_NeoPixel.cpp
+++ Adafruit_NeoPixel.cpp
@@ -108,9 +108,10 @@ extern "C" void ICACHE_RAM_ATTR espShow(
   uint8_t pin, uint8_t *pixels, uint32_t numBytes, uint8_t type);
 #endif // ESP8266
 
-void Adafruit_NeoPixel::show(void) {
+void Adafruit_NeoPixel::show(uint8_t *pixels) {
 
-  if(!pixels) return;
+  if (!pixels) pixels = this->pixels;
+  if (!pixels) return; 
 
   // Data latch = 50+ microsecond pause in the output stream.  Rather than
   // put a delay at the end of the function, the ending time is noted and
--- Adafruit_NeoPixel.h
+++ Adafruit_NeoPixel.h
@@ -124,7 +124,7 @@ class Adafruit_NeoPixel {
 
   void
     begin(void),
-    show(void),
+    show(uint8_t *pixels = NULL),
     setPin(uint8_t p),
     setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b),
     setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t w),
